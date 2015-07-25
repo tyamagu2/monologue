122b2f9eea7ddcd339807e02c1eaab84b309d134
tyamagu2 <tyamagu2@gmail.com>
2015-07-25 20:27:47 +0900

    Use react/flux

---
+++ client/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MonologueApp from './components/MonologueApp';
+window.React = React;
+
+React.render(
+  <MonologueApp />,
+  document.getElementById('app')
+);
---
+++ client/js/components/MonologueApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class MonologueApp extends React.Component {
+  constructor(...args) {
+    super(...args);
+  }
+  render() {
+    return (
+      <div className="monologue-app">
+        Monologue
+      </div>
+    );
+  }
+}
